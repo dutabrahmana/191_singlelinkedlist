bf667de9c00d8ac332acd7592aea500b523071e2
dutabrahmana <dutalinggau25@gmail.com>
2024-04-25 09:47:29 +0700

    Membuat kondisi if

--- Praktikum algo/Praktikum algo.cpp
+++ Praktikum algo/Praktikum algo.cpp
@@ -7,7 +7,7 @@ struct Node {
 	Node* next;
 
 };
-Node* STRAT = NULL;
+Node* START = NULL;
 
 void addNode() {
 	int nim;
@@ -19,4 +19,11 @@ void addNode() {
 	cin >> nama;
 	nodeBaru->noMhs = nim;
 	nodeBaru->name = nama;
+
+	if (START == NULL || nim <= START->noMhs) {
+		if (START != NULL && nim == START->noMhs) {
+
+		}
+
+	}
 }
